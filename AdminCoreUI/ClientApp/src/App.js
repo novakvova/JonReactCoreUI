0bf14c7db37baefeb35b45c7ab6a22505156cc69
novakvova <novakvova@gmail.com>
2019-07-11 20:51:36 +0300

    Begin wokr site

---
+++ AdminCoreUI/ClientApp/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { Route } from 'react-router';
+//import Layout from './components/Layout';
+import Home from './components/Home';
+import Counter from './components/Counter';
+import FetchData from './components/FetchData';
+
+export default () => (
+  <h1>Hello Peter</h1>
+  // <Layout>
+  //   <h1>Hello Peter</h1>
+  //  <Route exact path='/' component={Home} />
+  //   <Route path='/counter' component={Counter} />
+  //   <Route path='/fetch-data/:startDateIndex?' component={FetchData} /> 
+  //  </Layout>
+);
